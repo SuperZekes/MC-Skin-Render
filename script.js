0b324baf2e6c0d219de353d80c346552a8d0c17a
SuperZekes <markandsheenaknowles@gmail.com>
2024-03-31 16:03:54 -0400

    Move css and javascript into their own files

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeSkin() {
+    const username = document.getElementById("usernameInput").value;
+    const skinFrame = document.getElementById("skinFrame");
+    const newSrc = `https://minerender.org/embed/skin/?skin=${username}&shadow=true`;
+    skinFrame.src = newSrc;
+}
+function downloadSkin() {
+    const username = document.getElementById("usernameInput").value;
+    const skinFrame = document.getElementById("skinFrame");
+    const newSrc = `https://minecraft.tools/download-skin/${username}`;
+    skinFrame.src = newSrc;
+}
